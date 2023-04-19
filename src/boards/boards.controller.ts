5780395faf6cab78655d3aeaa7201de268378b5a
chan <cham9994@gmail.com>
2023-04-19 23:26:09 +0900

    feat: 특정 id를 가진 board 삭제

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common'
 import { BoardsService } from './boards.service'
 import { Board } from './board.model'
 import { CreateBoardDto } from './dto/create-board.dto'
@@ -24,5 +24,11 @@ export class BoardsController {
     console.log(id)
 
     return this.boardsService.getBoardById(id)
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    console.log(id)
+    this.boardsService.deleteBoard(id)
   }
 }
