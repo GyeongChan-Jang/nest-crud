804191d7a97f66795dc1f24a1ad4768c9e310b02
GyeongChan-Jang <cham9994@gmail.com>
2023-04-27 19:48:45 +0900

    feat: 로그인시 JWT 생성 및 반환

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,23 @@ import { AuthService } from './auth.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserRepository } from './user.repository'
 import { User } from './user.entity'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    // Passport 모듈 등록
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // Jwt 모듈 등록
+    JwtModule.register({
+      secret: 'Secret1234',
+      signOptions: {
+        // 토큰의 만료시간: 1시간
+        expiresIn: 60 * 60,
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
 })
