19bc9ee9c39e9d5e092ac9fa9d72e3336840e5e5
chan <cham9994@gmail.com>
2023-04-30 22:08:00 +0900

    feat: 인증된 유저만 boards 요청 접근 가능

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -7,6 +7,7 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common'
@@ -15,8 +16,10 @@ import { BoardStatus } from './board-status.enum'
 import { CreateBoardDto } from './dto/create-board.dto'
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 import { Board } from './board.entity'
+import { AuthGuard } from '@nestjs/passport'
 
 @Controller('boards')
+@UseGuards(AuthGuard()) // 로그인된(인증된) 사용자만 접근 가능
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
   @Get()
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,10 +4,11 @@ import { BoardsService } from './boards.service'
 import { BoardRepository } from './board.repository'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Board } from './board.entity'
+import { AuthModule } from 'src/auth/auth.module'
 
 @Module({
   // 생성한 Repository를 다른 곳에서도 사용할 수 있기 위해서 imports
-  imports: [TypeOrmModule.forFeature([Board])],
+  imports: [TypeOrmModule.forFeature([Board]), AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService, BoardRepository],
 })
